984209ce5e72c2d6423281867864147e4431cee9
vinit-jain713 <jainvinit713@gmail.com>
2026-06-07 07:52:30 +0530

    Initial Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+// import React, { ReactNode } from 'react'
+
+// interface AuthLayoutProps {
+//     children: ReactNode;
+// }
+// const AuthLayout = ({children}: AuthLayoutProps) => {
+//   return (
+//     <div className='flex justify-center pt-25'>{children}</div>
+//   )
+// }
+
+// export default AuthLayout;
---
+++ components/header.tsx
@@ -0,0 +1,47 @@
+import { Show, SignIn, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { LayoutDashboard, PenBox } from 'lucide-react'
+
+const Header = () => {
+  return (
+    <div className='w-full bg-white/80 backdrop-blur-md z-50 border-b fixed top-0'>
+      <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
+        <Link href='/'>
+          <Image src={"/mainlogo.png"} alt='Niova Logo' height={60} width={150} className='h-12 w-auto object-contain' />
+        </Link>
+
+        <div className='flex items-center space-x-4'>
+          <Show when="signed-in">
+            <Link href={"/dashboard"} className='text-gray-600 hover:text-blue-500 flex items-center gap-2'>
+              <Button variant="outline">
+                <LayoutDashboard size={18}/>
+                <span className='hidden md:inline'>Dashboard</span>
+              </Button>
+            </Link>
+
+            <Link href={"/transaction/create"}>
+              <Button className={'flex items-center gap-2'}>
+                <PenBox size={18}/>
+                <span className='hidden md:inline'>Add Transaction</span>
+              </Button>
+            </Link>
+          </Show>
+
+          <Show when="signed-out">
+            <SignInButton forceRedirectUrl={"/dashboard"}>
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </Show>
+
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
